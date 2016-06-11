ca5781e85255a9c8bd7f401590720689f8a35b79
vato20071 <vato20071@gmail.com>
2016-06-11 19:40:35 +0400

    Fix package name in web.xml

--- test/PingTest.java
+++ test/PingTest.java
@@ -1,5 +1,32 @@
+import ge.edu.freeuni.sdp.service.scheduler.temperature.PingService;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by vato on 6/11/16.
  */
-public class PingTest {
+public class PingTest extends JerseyTest {
+
+    @Override
+    protected Application configure()
+    {
+        return new ResourceConfig(PingService.class);
+    }
+
+    @Test
+    public void TestPingReturns200() throws Exception {
+
+        Response ping = target("ping")
+                .request().get();
+
+
+        int status = ping.getStatus();
+        assertEquals(200, status);
+    }
 }
